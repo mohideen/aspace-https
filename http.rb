e47f759c06ec796da36e4a8675c7b1ce831c13c9
Mohamed Abdul Rasheed <mohideen@umd.edu>
2017-05-22 13:03:46 -0400

    Added openssl gem and client cert config

--- http.rb
+++ http.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require 'openssl'
 class Net::HTTP
 
   singleton_class.send(:alias_method, :start_orig, :start)
@@ -6,7 +7,11 @@ class Net::HTTP
   class << self 
     def start(address, *arg, &block)
       opt = Hash.try_convert(arg[-1]) ? arg.pop : {} 
-      opt[:use_ssl] = ( [ 443, 8443 ].include?(arg.first) ) 
+      opt[:use_ssl] = ( [ 443, 8443 ].include?(arg.first) )
+      opt[:ssl_version] = :TLSv1_2
+      pem_file = File.read(AppConfig[:client_cert_location])
+      opt[:cert] = OpenSSL::X509::Certificate.new(pem_file)
+      opt[:key] = OpenSSL::PKey::RSA.new(pem_file)
       arg << opt 
       start_orig(address, *arg, &block )
     end
